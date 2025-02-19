c2d15e75da51d24565f1a96c081cce30171cd7c2
Daniel Zhu Zhang <danielzhuzhang@gmail.com>
2025-02-19 18:28:34 -0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    reactStrictMode: true,
+    swcMinify: true,
+    async rewrites() {
+        return [
+            {
+                source: "/api/:path*", // Any request to /api/* will be proxied
+                destination: "https://temp-3-923n.onrender.com/api/:path*", // Change 5000 to your Express server port
+            },
+        ];
+    },
+};
 
 export default nextConfig;
-
